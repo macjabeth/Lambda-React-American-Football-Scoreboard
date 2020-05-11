bbe52fc46aee6be5af255a7275fb69fa5d70b6ff
macjabeth <macbethjonathan@gmail.com>
2020-05-11 13:47:13 -0400

    break app into multiple components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,40 @@ import React, { useState } from 'react';
 import BottomRow from './BottomRow';
 import './App.css';
 
+function Scoreboard(props) {
+  return (
+    <section className="scoreboard">
+      <div className="topRow">
+        <div className="home">
+          <h2 className="home__name">Lions</h2>
+          <div className="home__score">{props.score.home}</div>
+        </div>
+        <div className="timer">00:03</div>
+        <div className="away">
+          <h2 className="away__name">Tigers</h2>
+          <div className="away__score">{props.score.away}</div>
+        </div>
+      </div>
+      <BottomRow />
+    </section>
+  );
+}
+
+function Controls(props) {
+  return (
+    <section className="buttons">
+      <div className="homeButtons">
+        <button className="homeButtons__touchdown" onClick={props.homeTouchdown}>Home Touchdown</button>
+        <button className="homeButtons__fieldGoal" onClick={props.homeFieldGoal}>Home Field Goal</button>
+      </div>
+      <div className="awayButtons">
+        <button className="awayButtons__touchdown" onClick={props.awayTouchdown}>Away Touchdown</button>
+        <button className="awayButtons__fieldGoal" onClick={props.awayFieldGoal}>Away Field Goal</button>
+      </div>
+    </section>
+  );
+}
+
 function App() {
   const [score, setScore] = useState({ home: 0, away: 0 });
 
@@ -11,30 +45,11 @@ function App() {
 
   return (
     <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-            <div className="home__score">{score.home}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{score.away}</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          <button className="homeButtons__touchdown" onClick={incrementScore('home', 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={incrementScore('home', 3)}>Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={incrementScore('away', 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={incrementScore('away', 3)}>Away Field Goal</button>
-        </div>
-      </section>
+      <Scoreboard score={score} />
+      <Controls homeTouchdown={incrementScore('home', 7)}
+                homeFieldGoal={incrementScore('home', 3)}
+                awayTouchdown={incrementScore('away', 7)}
+                awayFieldGoal={incrementScore('away', 3)} />
     </div>
   );
 }
