f5fe59aa3b54fdbd1cfb3a50328a19a35b37de3f
macjabeth <macbethjonathan@gmail.com>
2020-05-11 14:08:19 -0400

    implement timer functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BottomRow from './BottomRow';
 import './App.css';
 
 function Scoreboard(props) {
+  const minutes = props.time.minutes.toString().padStart(2, '0');
+  const seconds = props.time.seconds.toString().padStart(2, '0');
+
   return (
     <section className="scoreboard">
       <div className="topRow">
@@ -10,7 +13,7 @@ function Scoreboard(props) {
           <h2 className="home__name">Lions</h2>
           <div className="home__score">{props.score.home}</div>
         </div>
-        <div className="timer">00:03</div>
+        <div className="timer">{minutes}:{seconds}</div>
         <div className="away">
           <h2 className="away__name">Tigers</h2>
           <div className="away__score">{props.score.away}</div>
@@ -38,6 +41,21 @@ function Controls(props) {
 
 function App() {
   const [score, setScore] = useState({ home: 0, away: 0 });
+  const [time, setTime] = useState({ seconds: 3, minutes: 0 });
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setTime(time => {
+        if (time.seconds === 59) {
+          return { minutes: time.minutes + 1, seconds: 0 };
+        } else {
+          return { ...time, seconds: time.seconds + 1 };
+        }
+      })
+    }, 1000);
+
+    return () => clearInterval(intervalID);
+  }, []);
 
   const incrementScore = (team, amount) => () => {
     setScore({ ...score, [team]: score[team] + amount });
@@ -45,7 +63,7 @@ function App() {
 
   return (
     <div className="container">
-      <Scoreboard score={score} />
+      <Scoreboard score={score} time={time} />
       <Controls homeTouchdown={incrementScore('home', 7)}
                 homeFieldGoal={incrementScore('home', 3)}
                 awayTouchdown={incrementScore('away', 7)}
